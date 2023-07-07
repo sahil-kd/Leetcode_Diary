37cfd2c9d199aed3b17e3d0d9d59b3c349b6d5be
Kaustav Das <kaustay@gmail.com>
2023-07-07 19:56:27 +0530

    added tips into dev guide

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@
     Core ts dev guide:
     a) run ts in watch mode using tsc -w or, npm run dev | Ctrl + C to exit mode
     b) after running tsc -w, add new terminal with '+' icon, now you can use git bash while
-       previous terminal takes care of watching changes in ts file on each save
+       previous terminal takes care of watching changes in ts file on each save, or set ts
+       watch mode on one terminal & use git bash cli app -> easier to switch
     c) use Prettier
     d) run npm start
 */
