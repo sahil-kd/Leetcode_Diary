d90483f6d27f1a8de88280c4bd14f0d3d649f0b0
Kaustav Das <kaustay@gmail.com>
2023-07-07 15:46:00 +0530

    configured tsconfig.json and package.json

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/*
+    Core ts file rules:
+    a) run ts in watch mode using tsc -w | Ctrl + C to exit mode
+    b) use Prettier
+*/
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+/*
+    Core ts file rules:
+    a) run ts in watch mode using tsc -w | Ctrl + C to exit mode
+    b) use Prettier
+*/
